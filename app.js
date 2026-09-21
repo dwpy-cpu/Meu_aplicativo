6fea7eb10433f75589a4497919c31dabc9cd4802
dwpy-cpu <dawidhdr@gmail.com>
2026-09-21 20:45:34 -0300

    Meu Aplicativo

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const nome = "Luna";
+const cidade = "São Paulo";
+const interesse = "Criar playlists";
+const aprender = "Tocar guitarra";
+const area = "Tecnologia";
+const message = `Eu estou fazendo curso de programação!`;
+
+console.log("====================");
+console.log("   Meu Aplicativo   ")
+console.log("====================");
+console.log(`nome: ${nome}`);
+console.log(`cidade: ${cidade}`);
+console.log(`interesse: ${interesse}`);
+console.log(`Quero aprender: ${aprender}`);
+console.log(`Área de interesse: ${area}`);
+console.log(message);
